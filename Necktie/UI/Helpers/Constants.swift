16ee83527ca7dc0a57913a50db8fc668aacf0d8d
Ondra Kandera <xergic@gmail.com>
2016-09-30 14:07:30 +0200

    Initial commit

---
+++ Necktie/UI/Helpers/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Necktie
+//
+//  Created by Ondra Kandera on 30/9/16.
+//  Copyright © 2016 Necktie. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Global constants
+struct Constant {
+    
+}
+
+/// Global colors
+struct Color {
+    static let necktieBlue = UIColor(red:0.15, green:0.24, blue:0.33, alpha:1.00)
+    static let necktieGreen = UIColor(red:0.00, green:0.77, blue:0.62, alpha:1.00)
+}
